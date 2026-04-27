91b40c8a8c9426d7c238d3d0544ba6d057e6ebc0
umniyudaa-bit <umniyudaa@gmail.com>
2026-04-27 13:12:41 +0700

    belajar mcr  periode

---
+++ app/Http/Controllers/FakultasController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fakultas;
+use Illuminate\Http\Request;
+
+class FakultasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //akses model fakultas 
+        $result = Fakultas::all(); //select * from fakultas
+        dd($result); //dump data 
+        // kirim data ke view
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Fakultas $fakultas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Fakultas $fakultas)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\PeriodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('fakultas', FakultasController::class);
+
+Route::resource('periode', PeriodeController::class);
